5c2e4d7e7fcc7b1ab4eb0319007f7ec379bf3a3e
Hữu Khánh <16661938+huukhanh@users.noreply.github.com>
2023-10-02 13:34:16 +0700

    Add fn docs

--- main.py
+++ main.py
@@ -1,23 +1,34 @@
 import requests
 import time
 
+# Personal access token for CircleCI API authentication
 personalToken = '4e28df029f5be5ad2ad20511299411593d12a571'
-slugs = [
-    'undodel/test_ci'
-]
-url = 'https://circleci.com/api/v2/'
-headers={"Content-Type":"application/json", 'Circle-Token': personalToken}
 
+# List of repository slugs (in the format 'owner/repo') to monitor
+slugs = ['undodel/test_ci']
+
+# Base URL for the CircleCI API
+url = 'https://circleci.com/api/v2/'
+
+# Headers for the HTTP requests
+headers = {"Content-Type": "application/json", 'Circle-Token': personalToken}
+
+
+# Function to send a GET request to the CircleCI API
 def sendGetRequst(path, *param):
     reqUrl = url + path
     result = requests.get(reqUrl.format(*param), headers=headers)
     return result
 
+
+# Function to send a POST request to the CircleCI API
 def sendPostRequst(path, *param):
     reqUrl = url + path
     result = requests.post(reqUrl.format(*param), headers=headers)
     return result
 
+
+# Function to retrieve all pipeline IDs for a given repository slug
 def getAllPipeLine(slug):
     path = 'project/github/{}/pipeline'.format(slug)
     reqResult = sendGetRequst(path)
@@ -25,6 +36,8 @@ def getAllPipeLine(slug):
         return [pipeline['id'] for pipeline in reqResult.json()['items']]
     return []
 
+
+# Function to retrieve workflow information for a specific pipeline
 def getWorkflowByPipeline(pipelineId):
     path = 'pipeline/{}/workflow'
     reqResult = sendGetRequst(path, pipelineId)
@@ -32,12 +45,16 @@ def getWorkflowByPipeline(pipelineId):
         return [{'id': pipeline['id'], 'status': pipeline['status']} for pipeline in reqResult.json()['items']]
     return []
 
+
+# Function to retrieve workflow information for multiple pipelines
 def getWorkflowByPipelines(pipelineIds):
     workflows = []
     for pipeline in pipelineIds:
         workflows += getWorkflowByPipeline(pipeline)
     return workflows
 
+
+# Function to retrieve job information for a specific workflow
 def getJobByWorkflow(workflowId):
     path = 'workflow/{}/job'
     reqResult = sendGetRequst(path, workflowId)
@@ -45,6 +62,8 @@ def getJobByWorkflow(workflowId):
         return [{'workflow_id': workflowId, 'approval_request_id': workflow['approval_request_id']} for workflow in reqResult.json()['items'] if workflow['type'] == 'approval']
     return []
 
+
+# Function to retrieve job information for multiple workflows
 def getJobByWorkflows(workflowIds):
     jobs = []
     for workflowId in workflowIds:
@@ -52,16 +71,22 @@ def getJobByWorkflows(workflowIds):
         jobs += getJobByWorkflow(workflowId)
     return jobs
 
+
+# Function to approve jobs that are on hold
 def approvalHoldingJob(workflowId, approvalRequestId):
     path = 'workflow/{}/approve/{}'
     reqResult = sendPostRequst(path, workflowId, approvalRequestId)
     if reqResult.status_code == 200:
-        print('{} was approval for build'.format(workflowId))
+        print('{} was approved for build'.format(workflowId))
 
+
+# Function to approve jobs that are on hold for multiple workflows
 def approvalHoldingJobs(jobs):
     for job in jobs:
         approvalHoldingJob(job['workflow_id'], job['approval_request_id'])
 
+
+# Main function to continuously monitor and approve on-hold jobs
 def main():
     while True:
         for slug in slugs:
@@ -77,4 +102,7 @@ def main():
                 pass
         time.sleep(5)
 
-main()
+
+# Entry point of the script
+if __name__ == "__main__":
+    main()
